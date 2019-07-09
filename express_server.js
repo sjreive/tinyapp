9d114d98eb3a07a925a4099d3c2ae5b683b49814
Sarah <sarahjreive@gmail.com>
2019-07-09 19:07:07 +0000

    added logout route

--- express_server.js
+++ express_server.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -32,12 +35,14 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  console.log(req.cookies);
+  let templateVars = { urls: urlDatabase, username: req.cookies.username };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { urls: urlDatabase, username: req.cookies.username };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -51,7 +56,22 @@ app.post("/login", (req, res) => {
   console.log(req.body);
   res.cookie('username', req.body.username);
   res.redirect('/urls');
-  
+  let templateVars = {
+    username: req.body.username
+  };
+  console.log(templateVars);
+  res.render("urls_index", templateVars);
+});
+
+app.post("/logout", (req, res) => {
+  console.log(req.body);
+  res.cookie('username', "");
+  res.redirect('/urls');
+  let templateVars = {
+    username: req.body.username
+  };
+  console.log(templateVars);
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
